5a50fdf74a4ef40569f5ef67791edbd36265c283
jinqiangqiang <vectorse@126.com>
2018-03-05 11:30:50 +0800

    去除jq引用

--- main.js
+++ main.js
@@ -1,89 +1,110 @@
-$(function () {
-	var workCont = '';
-	var workTime = 0;
-	var restTime = 0;
-	var start = 0;
-	var end = 0;
-	var current = 0;
-	var timer = null;
-	var $clock = $('.clock');
+window.onload = function () {
+
+	let startTime = 0; //开始工作时间
+	let workContent = ''; //工作内容
+	let timer = 0; //定时器
+	let doc = document;
 
 	//获取提醒权限
 	Notification.requestPermission();
 
-	//开始工作
-	$('.work-btn').on('click', function () {
-		start = Date.now();
-		workCont = $('.work-content').val();
-		workTime = +$('.work-timer').val(); 
-		restTime = +$('.rest-timer').val();
-		current = (workTime + restTime) * 60;
-		timer = setInterval(function () {
-			--current;
-			$clock.text(sec_str(current));
-			if (current == restTime * 60) {
-				var n = new Notification('提醒', {
-					body: '休息时间到了',
-					tag: 'vector',
-					requireInteraction: false
-				});
+	//秒转字符串时间格式
+	let sec_str = function (seconds) {
+		let m = Math.floor(seconds / 60);
+		let s = seconds % 60;
+		let h = Math.floor(m / 60);
+		m = m % 60;
+		return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
+	};
+
+	//notification提醒
+	let notify = function () {
+		let n = new Notification('提醒', {
+			body: '休息时间到了',
+			tag: 'vector',
+			requireInteraction: false
+		});
+	};
+
+	//date转字符串
+	let timeFormat = function (time) {
+		let date = new Date(time);
+		let h = date.getHours();
+		let m = date.getMinutes();
+		return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m);
+	};
+
+	//设置工作记录
+	let setWorkList = function (str) {
+		if (str) {
+			let result = localStorage.workList;
+			if (result) {
+				result = result + '|' + str;
+			} else {
+				result = str;
 			}
-			if (current == 0) {
+			localStorage.workList = result;
+		}
+	};
+
+	//显示工作记录列表
+	let showWorkList = function () {
+		let result = localStorage.workList;
+		let list = result.split('|');
+		let html = '';
+		for (let i = 0, len = list.length; i < len; i++) {
+			html += '<li>' + list[i] + '</li>';
+		}
+		doc.getElementsByClassName('records')[0].innerHTML = html;
+	};
+
+	//重置工作记录
+	let resetWorkList = function () {
+		localStorage.workList = '';
+	};
+
+	//开始工作
+	doc.getElementById('work-btn').onclick = function () {
+
+		let workTime = +(doc.getElementsByClassName('work-timer')[0].value);
+		let restTime = +(doc.getElementsByClassName('rest-timer')[0].value);
+		let total = (workTime + restTime) * 60;
+		startTime = Date.now();
+		workContent = doc.getElementsByClassName('work-content')[0].value;
+
+		timer = setInterval(function () {
+			total--;
+			doc.getElementsByClassName('clock')[0].innerText = sec_str(total);
+			if (total == restTime * 60) {
+				notify();
+			}
+			if (total == 0) {
 				clearInterval(timer);
 				timer = null;
 			}
 		}, 1000);
-	});
+	};
 
 	//停止工作
-	$('.stop-btn').on('click', function () {
-		end = Date.now();
-		$clock.text('00:00:00');
+	doc.getElementById('stop-btn').onclick = function () {
+
+		let endTime = Date.now();
+
+		doc.getElementsByClassName('clock')[0].innerText = '00:00:00';
 		if (timer) {
 			clearInterval(timer);
+			timer = 0;
 		}
-		setWorkList(workCont + '-------------from ' + timeFormat(start) + ' to ' + timeFormat(end));
+		setWorkList(workContent + '-------------from ' + timeFormat(startTime) + ' to ' + timeFormat(endTime));
 		showWorkList();
-	});
+	};
 
 	//清除工作记录
-	$('.clear-btn').on('click', function () {
+	doc.getElementById('clear-btn').onclick = function () {
 		resetWorkList();
 		showWorkList();
-	});
-	var sec_str = function (s) {
-		var m = Math.floor(s / 60);
-		var s = s % 60;
-		var h = Math.floor(m / 60);
-		m = m % 60;
-		return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m) + ':' + (s < 10 ? '0' + s : s);
 	};
-	var setWorkList = function (str) {
-		var result = localStorage.workList;
-		if (result) {
-			result = result + '|' + str;
-		} else {
-			result = str;
-		}
-		localStorage.workList = result;
-	};
-	var showWorkList = function () {
-		var result = localStorage.workList;
-		var list = result.split('|');
-		var html = '';
-		for (var i = 0, len = list.length; i < len; i++) {
-			html += '<li>' + list[i] + '</li>';
-		}
-		$('ul.records').html(html);
-	};
-	var resetWorkList = function () {
-		localStorage.workList = '';
-	};
-	var timeFormat = function (time) {
-		var date = new Date(time);
-		var h = date.getHours();
-		var m = date.getMinutes();
-		return (h < 10 ? '0' + h : h) + ':' + (m < 10 ? '0' + m : m);
-	};
+
+	//页面初始化显示工作记录
 	showWorkList();
-});
+};
